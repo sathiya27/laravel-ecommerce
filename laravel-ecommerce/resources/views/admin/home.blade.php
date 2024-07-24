dc6cec770764ca4d932058b1593cb115a69a6d9f
SathiyaS <selan.sathiya16@gmail.com>
2024-07-24 17:32:20 +0800

    Added admin page

--- laravel-ecommerce/resources/views/admin/home.blade.php
+++ laravel-ecommerce/resources/views/admin/home.blade.php
@@ -1,15 +1,24 @@
-<x-app-layout>
-
-</x-app-layout>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>admin</h1>
-</body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Corona Admin</title>
+    <!-- plugins:css -->
+    @include("admin.css")
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+      @include('admin.header')
+        <!-- partial -->
+        @include('admin.body')
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.js')
+    <!-- End custom js for this page -->
+  </body>
 </html>
